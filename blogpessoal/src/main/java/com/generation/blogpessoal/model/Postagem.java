a17d689743f7cefb33b511e975c4fcf25a7e8934
Andreia <deia_kas@hotmail.com>
2022-06-30 19:47:26 -0300

    Adicionando o metodo put e delete

--- blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -13,24 +13,28 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 
-@Entity
-@Table(name="postagem")
+@Entity //vai transformar o objeto em uma tabela no banco de dado
+@Table(name="postagem") // coloca o nome na tabela
 public class Postagem {
 	
-	@Id
+	@Id //chave primaria o mesmo que primary key, sempre acima da aplicação
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
-	@NotNull
+	@NotNull //obriga a preencher o campo
 	@Size(min = 5, max = 100)
 	private String titulo;
 	
 	@NotNull
-	@Size(min = 10, max = 500)
+	//message para informar ao usuario min e max de letras
+	@Size(min = 10, max = 500) //delimita o tamanho do conteudo, não é obrigatorio ter
 	private String texto;
 	
+	//@updatetimestamp versão mais atualizada do codigo
 	@Temporal(TemporalType.TIMESTAMP)
-	private Date date = new java.sql.Date(System.currentTimeMillis());
+	
+	// LocalDateTime outra opção de inserir data automaticamente
+	private Date date = new java.util.Date(System.currentTimeMillis());
 	
 	public long getId() {
 		return id;
